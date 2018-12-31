93df390d85d81971f86ac3052925201afade1722
GeorgieSantiago <milestoneartisan@gmail.com>
2018-12-31 12:55:44 -0500

    simple laravel application boilerplate

---
+++ resources/js/src/components/navigation/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export const Header = () => { return(
+  <nav className='navbar navbar-expand-md navbar-light navbar-laravel'>
+  <div className='container'>
+    <Link className='navbar-brand' to='/'>Tasksman</Link>
+  </div>
+</nav>
+)
+}
